1769bacc048049aed5220c8f910b938950413e3b
Paul Straw <paulstraw@paulstraw.com>
2016-08-31 17:06:15 -0700

    Bump version to 0.1.3

--- Sources/ImgixClient.swift
+++ Sources/ImgixClient.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 @objc public class ImgixClient: NSObject {
-    static public let VERSION = "0.1.2"
+    static public let VERSION = "0.1.3"
 
     public let host: String
     public var useHttps: Bool = true
