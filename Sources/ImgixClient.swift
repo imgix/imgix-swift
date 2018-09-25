33e9ac887e329b0587a637033db5e716662a0a17
Nathan Heskia <nathan@imgix.com>
2018-09-25 13:26:16 -0700

    Use allKeys

--- Sources/ImgixClient.swift
+++ Sources/ImgixClient.swift
@@ -124,7 +124,7 @@ import Foundation
             params.setValue("swift-" + ImgixClient.VERSION, forKey: "ixlib")
         }
 
-        for key in params.keys.sorted() {
+        for key in params.allKeys.sorted() {
             let stringKey = String(describing: key)
             var stringVal = String(describing: params[val])
 
