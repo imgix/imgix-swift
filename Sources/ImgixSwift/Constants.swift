fcd8a57e680f019da91b7658dc4b07478f0d7be4
Alice Lawrie <Alice.lawrie@imgix.com>
2022-10-20 13:18:47 -0400

    chore(constants): rolled constants into client

--- Sources/ImgixSwift/Constants.swift
+++
@@ -1,2 +0,0 @@
-// regex pattern used to determine if a domain is valid
-let _domainRegex = #"^(?:[a-z\d\-_]{1,62}\.){0,125}(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)[a-z\d]{1,63}$"#;
--- Sources/ImgixSwift/ImgixClient.swift
+++ Sources/ImgixSwift/ImgixClient.swift
@@ -5,6 +5,9 @@
 
 import Foundation
 
+// regex pattern used to determine if a domain is valid
+let _domainRegex = #"^(?:[a-z\d\-_]{1,62}\.){0,125}(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)[a-z\d]{1,63}$"#;
+
 @objc open class ImgixClient: NSObject {
     @objc static public let VERSION = "1.1.3"
 
